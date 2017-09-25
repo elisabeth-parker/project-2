18f5046e4359cf6cfb8e0986052647616b8ccf61
elisabeth-parker <elabeth@live.unc.edu>
2017-09-25 19:29:51 -0400

    finished section 1

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         direction: 'vertical',
         verticalCentered: true,
         sectionsColor: [],
-        anchors: [],
+        anchors: ['page1', 'page2', 'page3', 'page4'],
         scrollingSpeed: 700,
         easing: 'swing',
         loopBottom: false,
@@ -15,7 +15,7 @@ $(document).ready(function() {
             'textColor': '#000',
             'bulletsColor': '#000',
             'position': 'right',
-            'tooltips': ['section1', 'section2', 'section3', 'section4']
+            'tooltips': ['', 'The Essentials', 'section3', 'section4']
         },
        	normalScrollElements: null,
         normalScrollElementTouchThreshold: 5,
@@ -23,16 +23,68 @@ $(document).ready(function() {
         keyboardScrolling: true,
         sectionSelector: '.section',
         animateAnchor: false,
-        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+        sectionsColor: ['white', '#ffaf62', '#ff9966', '#e17960', '#cd545f', '#ab175e'],
 
 		//events
 		onLeave: function(index, nextIndex, direction){},
 		afterLoad: function(anchorLink, index){},
 		afterRender: function(){},
 	});
-  $('#subtitle').fadeIn(2000);
+  $('#subtitle').fadeIn(2500);
+  $('.hide').hide();
+  $()
 });
 
-$('.body').mousemove(function(event) {
-  $('#subtitle').fadeIn();
-});
+function sectionSetup(x) {
+  var title = x.getElementsByClassName("section-title");
+  $(title).html("");
+  $('.hide').fadeIn(1000);
+}
+
+var current = 0;
+var leftButton = $("#lr a").get(0);
+var rightButton = $("#lr a").get(1);
+
+leftButton.onclick = function() {previousText()};
+rightButton.onclick = function() {nextText()};
+
+function previousText() {
+  if(current==0) {
+  }
+  else {
+    current-=1;
+    displayText(current);
+  }
+}
+
+function nextText() {
+  console.log('nextText is running');
+  console.log(current);
+  if(current==2) {
+  }
+  else {
+    current+=1;
+    displayText(current);
+  }
+}
+
+function displayText(x) {
+  console.log('displayText is running')
+  var text, image;
+  switch (x) {
+    case 0:
+      text = "Ultimate frisbee is a bit like a hybrid between soccer and football. It's played with seven people per team on a grass, turf or sand field. Players throw the disc to one another and score by catching it in the endzone."
+      image = "images/intro1.png";
+      break;
+    case 1:
+      text = "Players typically specialize into roles called handlers and cutters. Handlers are like quarterbacks in football, and cutters are receivers."
+      image = "images/intro2.png";
+      break;
+    case 2:
+      text = "Ultimate is self-officiated, meaning players have to make their own calls. These include fouls (ultimate is non-contact), picks, and travels. Generally, the opposing player can contest the call being made."
+      image = "images/intro3.png";
+  }
+  console.log(text);
+  $('#intro').text(text);
+  $('#intro-image').attr("src",image);
+}
