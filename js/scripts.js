aec29afe9e34b7085c86e315337debce1892a935
elisabeth-parker <elabeth@live.unc.edu>
2017-09-27 15:40:40 -0400

    working on divisions page

--- js/scripts.js
+++ js/scripts.js
@@ -32,15 +32,21 @@ $(document).ready(function() {
 	});
   $('#subtitle').fadeIn(2500);
   $('.hide').hide();
-  $()
+  $('.tooltip').tooltipster({
+    interactive: 'true',
+    side: 'left'
+  });
+
 });
 
+// creates title to content transition when user moves to new page
 function sectionSetup(x) {
   var title = x.getElementsByClassName("section-title");
   $(title).html("");
   $('.hide').fadeIn(1000);
 }
 
+// functionality for essentials content
 var current = 0;
 var leftButton = $("#lr a").get(0);
 var rightButton = $("#lr a").get(1);
@@ -75,16 +81,22 @@ function displayText(x) {
     case 0:
       text = "Ultimate frisbee is a bit like a hybrid between soccer and football. It's played with seven people per team on a grass, turf or sand field. Players throw the disc to one another and score by catching it in the endzone."
       image = "images/intro1.png";
+      document.getElementById("left").style.opacity = "0";
+      document.getElementById("right").style.opacity = "1";
       break;
     case 1:
       text = "Players typically specialize into roles called handlers and cutters. Handlers are like quarterbacks in football, and cutters are receivers."
       image = "images/intro2.png";
+      document.getElementById("left").style.opacity = "1";
+      document.getElementById("right").style.opacity = "1";
       break;
     case 2:
       text = "Ultimate is self-officiated, meaning players have to make their own calls. These include fouls (ultimate is non-contact), picks, and travels. Generally, the opposing player can contest the call being made."
       image = "images/intro3.png";
+      document.getElementById("right").style.opacity = "0";
+      document.getElementById("left").style.opacity = "1";
   }
-  console.log(text);
+  // console.log(text);
   $('#intro').text(text);
   $('#intro-image').attr("src",image);
 }
